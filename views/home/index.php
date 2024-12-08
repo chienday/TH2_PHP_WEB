e7cb0a9bb48e0dc21392fb8e5e0f5fd0b682bf96
PhuocNN123 <138138943+PhuocNN123@users.noreply.github.com>
2024-12-08 22:56:24 +0700

    Update index.php

--- views/home/index.php
+++ views/home/index.php
@@ -3,13 +3,65 @@ $noidung = [
     [
         'title' => 'Khởi công khối phòng học điểm trường ở Hà Tĩnh',
         'description' =>'Công trình khối phòng học chức năng sẽ là nơi dạy và học của thầy trò Trường tiểu học Quang Vĩnh, đồng thời cũng là điểm tránh trú an toàn khi mưa lũ về.',
+        'img'=>'https://cdn.tuoitre.vn/zoom/260_163/471584752817336320/2023/12/6/untitled2-17018246362671869328438-0-0-823-1316-crop-1701826012086582483107.png',
         'url'=>'https://tuoitre.vn/khoi-cong-khoi-phong-hoc-diem-truong-vuot-lu-o-ha-tinh-20231206083052028.htm',
     ],
     [
         'title' =>'Flick phân bua sau khi bị truất quyền chỉ đạo',
         'description'=>'HLV Barca Hansi Flick cho rằng khác biệt với văn hóa Tây Ban Nha khiến ông phải nhận thẻ đỏ vì phản ứng trong trận hòa Real Betis 2-2 ở La Liga ngày 7/12.',
+        'img'=>'https://i1-thethao.vnecdn.net/2024/12/08/screenshot-2024-12-08-at-17-29-6135-2028-1733653908.png?w=220&h=132&q=100&dpr=1&fit=crop&s=7og71cnB9oDW7nF7tuFONQ',
         'url'=>'https://vnexpress.net/flick-phan-bua-sau-khi-bi-truat-quyen-chi-dao-4825348.html',
-    ]
+    ],
+    [
+        'title'=>'1,5 triệu sinh viên tham gia Lớp học đáp vui',
+        'description'=>'Lớp học đáp vui" diễn ra tại 300 trường cao đẳng và đại học trên cả nước từ tháng 9 đến nay nằm trong chuỗi sự kiện Jollibee School Tour. ',
+        'img'=>'https://i1-vnexpress.vnecdn.net/2024/11/19/jo-1732007044-6647-1732007079.jpg?w=500&h=300&q=100&dpr=1&fit=crop&s=KUlmcMXdGoxcsz_1ucl0Jw',
+        'url'=>'https://vnexpress.net/1-5-trieu-sinh-vien-tham-gia-lop-hoc-dap-vui-4817838.html',
+    ],
+    [
+        'title'=>'Cầu nối mở rộng liên kết để phát triển du lịch',
+        'description'=>'(Chinhphu.vn) - Những năm qua, ngành Du lịch tỉnh Thái Nguyên đã có nhiều chuyển biến tích cực; các khu, điểm du lịch được quy hoạch, xây dựng và đi vào hoạt động với hệ thống hạ tầng kỹ thuật ngày càng hoàn thiện; chất lượng sản phẩm, mạng lưới cơ sở kinh doanh dịch vụ phát triển nhanh, thu hút ngày càng nhiều du khách… Kết quả này có đóng góp không nhỏ của Hiệp hội Du lịch tỉnh.',
+        'img'=>'https://bcp.cdnchinhphu.vn/zoom/585_365/334894974524682240/2024/12/8/chehn-17336666943281769305427-0-60-675-1140-crop-17336667079431925777916.jpg',
+        'url'=>'https://baochinhphu.vn/cau-noi-mo-rong-lien-ket-de-phat-trien-du-lich-102241208213416239.htm',
+    ],
+    [
+        'title'=>'5 xe va chạm, cầu Thăng Long ùn tắc',
+        'description'=>'Hà NộiBa xe tải và hai xe bốn chỗ va chạm liên hoàn trên đường dẫn lên cầu Thăng Long khiến vành đai 3 ùn tắc kéo dài 5 km',
+        'img'=>'https://i1-vnexpress.vnecdn.net/2024/10/05/z5898467658252-53d553d23405132-7623-2358-1728101609.jpg?w=500&h=300&q=100&dpr=1&fit=crop&s=X7xe24-aj83UP16LvHbsJw',
+        'url'=>'https://vnexpress.net/5-xe-va-cham-cau-thang-long-un-tac-4800534.html',
+    ],
+    [
+        'title'=>'Đề xuất BHYT chi trả 28 loại thuốc ung thư điều trị đích',
+        'description'=>'Các cơ sở khám chữa bệnh và đơn vị dược phẩm đề xuất bổ sung 75 thuốc mới vào danh mục BHYT, trong đó có 28 thuốc điều trị đích bệnh ung thư.',
+        'img'=>'https://i1-suckhoe.vnecdn.net/2024/12/05/233a8602-1733401056-1733401092-4662-1733401133.jpg?w=500&h=300&q=100&dpr=1&fit=crop&s=ADlzdPLB-BIr7iUMGzYAYw',
+        'url'=>'https://vnexpress.net/de-xuat-bhyt-chi-tra-28-loai-thuoc-ung-thu-dieu-tri-dich-4824379.html',
+    ],
+    [
+        'title'=>'Tư duy lý luận của Đảng về xây dựng mô hình chủ nghĩa xã hội ở Việt Nam từ năm 1991 đến nay',
+        'description'=>'',
+        'img'=>'https://www.tapchicongsan.org.vn/documents/20182/430193601/TBT+To+Lam+tham+dao+con+co.jpg/be375054-3c23-49ef-9d35-c70eff5d9500?t=1732872002687',
+        'url'=>'https://www.tapchicongsan.org.vn/web/guest/chinh-tri-xay-dung-dang/-/2018/1009102/tu-duy-ly-luan-cua-dang-ve-xay-dung-mo-hinh-chu-nghia-xa-hoi-o-viet-nam-tu-nam-1991-den-nay.aspx',
+    ],
+    [
+        'title'=>'Học Công nghệ thông tin hay Điện tử viễn thông dễ tìm việc tốt?',
+        'description'=>'Có phải Điện tử viễn thông khó, tốn nhiều thời gian, trong khi ngành Công nghệ thông tin cạnh tranh, dễ bị đào thải?.
+
+',
+        'img'=>'https://i1-vnexpress.vnecdn.net/2024/12/04/hocnganhcongnghethongtinhinh2-6310-9075-1733324228.jpg?w=680&h=408&q=100&dpr=1&fit=crop&s=ZZvwFidEn-Z9j5ZxfCNv5Q',
+        'url'=>'https://vnexpress.net/hoc-cong-nghe-thong-tin-hay-dien-tu-vien-thong-de-tim-viec-tot-4823947.html',
+    ],
+    [
+        'title'=>'Biểu dương 291 phụ nữ cao tuổi tiêu biểu trong xây dựng gia đình văn hóa, ấm no, hạnh phúc',
+        'description'=>'',
+        'img'=>'https://photo-baomoi.bmcdn.me/w250_r3x2/2024_12_07_14_50922291/2d3fb7264069a937f078.jpg.webp',
+        'url'=>'https://baomoi.com/bieu-duong-291-phu-nu-cao-tuoi-tieu-bieu-trong-xay-dung-gia-dinh-van-hoa-am-no-hanh-phuc-c50922291.epi',
+    ],
+    [
+        'title'=>'Nga nêu điều kiện then chốt để chấm dứt xung đột Ukraine',
+        'description'=>'',
+        'img'=>'https://photo-baomoi.bmcdn.me/w250_r3x2/2024_12_08_474_50930214/2beaea3b1874f12aa865.jpg.webp',
+        'url'=>'https://baomoi.com/nga-neu-dieu-kien-then-chot-de-cham-dut-xung-dot-ukraine-c50930214.epi',
+    ],
     ];
 ?>
 <!DOCTYPE html>
@@ -83,6 +135,7 @@ $noidung = [
                                 <div class="card-body">
                                     <h5 class="card-title"><?php echo ($index + 1) . '. ' . htmlspecialchars($data['title']); ?></h5>
                                     <p class="card-text"><?php echo htmlspecialchars($data['description']); ?></p>
+                                    <img src="<?php echo ($data['img']); ?>" alt="" class="flower-image">
                                     <a href="<?php echo htmlspecialchars($data['url']); ?>" class="btn btn-primary" target="_blank">Xem chi tiết</a>
                                 </div>
                             </div>
