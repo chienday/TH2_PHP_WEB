2c25f848aa93dad6d8dd48682e4db512a22f4cee
chienday <144938986+chienday@users.noreply.github.com>
2024-12-08 22:07:41 +0700

    Update Tintuc.sql

--- Tintuc.sql
+++ Tintuc.sql
@@ -26,3 +26,41 @@ CREATE TABLE news (
 
 insert into users (id, username, password, role) values (1, 'chiendaynha123', 'chienday22', '1');
 insert into users (id, username, password, role) values (2, 'chienuser', 'chien11', '0');
+INSERT INTO categories (id, name) 
+VALUES (1, 'Mới nhất');
+INSERT INTO categories (id, name) 
+VALUES (2, 'Hôm qua');
+INSERT INTO categories (id, name) 
+VALUES (3, 'Hôm kia');
+
+INSERT INTO news (id, title, content, image, created_at, category_id) 
+VALUES (1, 'Tin tức phòng học', 'Nội dung bài viết 1', 'image1.jpg', NOW(), 1);
+
+INSERT INTO news (id, title, content, image, created_at, category_id) 
+VALUES (2, 'Tin tức thể thao', 'Nội dung bài viết 2', 'image2.jpg', NOW(), 2);
+
+INSERT INTO news (id, title, content, image, created_at, category_id) 
+VALUES (3, 'Tin tức sinh viên', 'Nội dung bài viết 3', 'image3.jpg', NOW(), 3);
+
+INSERT INTO news (id, title, content, image, created_at, category_id) 
+VALUES (4, 'Tin tức nhà nước', 'Nội dung bài viết 4', 'image4.jpg', NOW(), 1);
+
+INSERT INTO news (id, title, content, image, created_at, category_id) 
+VALUES (5, 'Tin tức giao thông', 'Nội dung bài viết 5', 'image5.jpg', NOW(), 2);
+
+INSERT INTO news (id, title, content, image, created_at, category_id) 
+VALUES (6, 'Tin tức bảo hiểm y tế', 'Nội dung bài viết 6', 'image6.jpg', NOW(), 3);
+
+INSERT INTO news (id, title, content, image, created_at, category_id) 
+VALUES (7, 'Tin tức đảng công sản', 'Nội dung bài viết 7', 'image7.jpg', NOW(), 1);
+
+INSERT INTO news (id, title, content, image, created_at, category_id) 
+VALUES (8, 'Tin tức tuyển sinh', 'Nội dung bài viết 8', 'image8.jpg', NOW(), 2);
+
+INSERT INTO news (id, title, content, image, created_at, category_id) 
+VALUES (9, 'Tin tức khen thưởng', 'Nội dung bài viết 9', 'image9.jpg', NOW(), 3);
+
+INSERT INTO news (id, title, content, image, created_at, category_id) 
+VALUES (10, 'Tin tức báo mới', 'Nội dung bài viết 10', 'image10.jpg', NOW(), 1);
+
+
