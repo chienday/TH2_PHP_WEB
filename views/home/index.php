effeb611ec7597887674a1d26473bb99b77eb0e9
PhuocNN123 <138138943+PhuocNN123@users.noreply.github.com>
2024-12-08 23:07:03 +0700

    Update index.php

--- views/home/index.php
+++ views/home/index.php
@@ -70,8 +70,16 @@ $noidung = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TLU News</title>
-    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        .flower-image
+        {
+            width: 100%;
+            height: 200px;
+            object-fit: cover;
+            border-radius: 5px;
+        }
+    </style>
 </head>
 <body>
     <!-- Header -->
